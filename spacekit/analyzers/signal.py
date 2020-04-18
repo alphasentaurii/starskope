dd11a0ab01dfb2919b13ebb35fa7672a84bebb61
Hakkeray <hakkeray@gmail.com>
2020-04-18 11:35:54 -0700

    updated Fusion Matrix

--- spacekit/analyzers/signal.py
+++ spacekit/analyzers/signal.py
@@ -119,54 +119,9 @@ class flux:
 
 
 
-### MAKE_SPECGRAM
-# create function for generating and saving spectograph figs
-@staticmethod
-def make_specgram(signal, Fs=None, NFFT=None, noverlap=None, mode=None,
-                  cmap=None, units=None, colorbar=False, 
-                  save_for_ML=False, fname=None,num=None,**kwargs):
-    import matplotlib
-    import matplotlib.pyplot as plt
-    if mode:
-        mode=mode
-    if cmap is None:
-      cmap='binary'
-
-    #PIX: plots only the pixelgrids -ideal for image classification
-    if save_for_ML == True:
-        # turn off everything except pixel grid
-        fig, ax = plt.subplots(figsize=(10,10),frameon=False)
-        fig, freqs, t, m = plt.specgram(signal, Fs=Fs, NFFT=NFFT, mode=mode,cmap=cmap);
-        plt.axis(False)
-        plt.show();
-
-        if fname is not None:
-            try:
-                if num:
-                    path=fname+num
-                else:
-                    path=fname
-                plt.savefig(fname+num,**pil_kwargs)
-            except:
-                print('Something went wrong while saving the img file')
-
-    else:
-        fig, ax = plt.subplots(figsize=(13,11))
-        fig, freqs, t, m = plt.specgram(signal, Fs=Fs, NFFT=NFFT, mode=mode,cmap=cmap);
-        plt.colorbar();
-        if units is None:
-            units=['Wavelength (λ)','Frequency (ν)']
-        plt.xlabel(units[0]);
-        plt.ylabel(units[1]);
-        if num:
-            title=f'Spectogram_{num}'
-        else:
-            title='Spectogram'
-        plt.title(title)
-        plt.show();
-
-    return fig, freqs, t, m
-
+# check for change in relative flux vs position 
+    # also subtract off bad pixels 
+    #dflux = pdcflux[~bad_data]/np.nanmedian(pdcflux[~bad_data])
 
 
 ### IN PROG 
@@ -179,23 +134,4 @@ def make_specgram(signal, Fs=None, NFFT=None, noverlap=None, mode=None,
 
     #     fig, axes = plt.subplots(ncols=3, figsize=figsize, **subplot_kws)
     #     axes = axes.flatten()
-
-
-# def fastFourier(signal, bins):
-    # """
-    # takes in array and rotates #bins to the left as a fourier transform
-    # returns vector of length equal to input array
-    # """
-    # import numpy as np
-    # # 
-    # arr = np.asarray(signal)
-    # fq = np.arange(signal.size/2+1, dtype=np.float)
-    # phasor = exp(complex(0.0, (2.0*np.pi)) * fq * bins / float(signal.size))
-    # phaser = np.exp(complex(0.0, (2.0*np.pi)) * fq * 1 / float(sig.size))
-# 
-    # ff = np.fft.irfft(phasor * np.fft.rfft(signal))
-    # 
-    # return ff
-    # 
-    # 
 # 
---
+++ spacekit/instruments/spectro.py
@@ -0,0 +1,15 @@
+
+``
+#measurements of the electromagnetic spectrum
+"""
+measurements of the electromagnetic spectrum
+Words ending in -scopy mean "looking at" whereas words in -metry mean "measurement of". 
+
+"""
+
+class Oscillator(object):
+    
+    pass
+    
+    def funcname(self, parameter_list):
+        raise NotImplementedError
