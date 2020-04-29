738bbe6b13a3db1bcf9cd9fa6b2cdf65c5b974c5
hakkeray <rukeine@gmail.com>
2020-04-28 20:04:10 -0700

    transform fns

--- spacekit/analyzers/signal.py
+++ spacekit/analyzers/signal.py
@@ -125,6 +125,26 @@ class Flux:
 
 
 ### IN PROG 
+# Python3 code to demonstrate 
+# Every Kth index Maximum in List 
+# using list comprehension + enumerate() + max() 
+
+# initializing list 
+test_list = [1, 4, 5, 6, 7, 8, 9, 12] 
+
+# printing the original list 
+print ("The original list is : " + str(test_list)) 
+
+# initializing K 
+K = 3
+
+# using list comprehension + enumerate() + max() 
+# Every Kth index Maximum in List 
+# max of every 3rd element 
+res = max([i for j, i in enumerate(test_list) if j % K == 0 ]) 
+
+# printing result 
+print ("The max of every kth element : " + str(res)) 
 
 
 
