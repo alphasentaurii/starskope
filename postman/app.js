005dd3adc542a660f8df97caeba649e806e451d5
Postman Integration <integration@postman.com>
2020-08-26 16:46:01 -0700

    Postman updated MAST via AWS 1.00 Commit reference: YzM4ZDdjNDItNTdkYi00NGRiLWJmZTctODlmYTcyYjYxNjRh

--- postman/app.js
+++ postman/app.js
@@ -9,99 +9,13 @@
   },
   "servers": [
     {
-      "url": "http://petstore.swagger.io/v1"
+      "url": "s3://stpubdata"
     }
-  ],
+  ],  
   "paths": {
-    "/user": {
-      "get": {
-        "summary": "Details about a user",
-        "operationId": "listUser",
-        "tags": [
-          "user"
-        ],
-        "parameters": [
-          {
-            "name": "id",
-            "in": "query",
-            "description": "ID of the user",
-            "required": true,
-            "schema": {
-              "type": "integer",
-              "format": "int32"
-            }
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "Details about a user",
-            "headers": {
-              "x-next": {
-                "description": "A link to the next page of responses",
-                "schema": {
-                  "type": "string"
-                }
-              }
-            },
-            "content": {
-              "application/json": {
-                "schema": {
-                  "$ref": "#/components/schemas/User"
-                }
-              }
-            }
-          },
-          "default": {
-            "description": "unexpected error",
-            "content": {
-              "application/json": {
-                "schema": {
-                  "$ref": "#/components/schemas/Error"
-                }
-              }
-            }
-          }
+    "/K2": {
+        "get": {
+            "query_criteria": ""
         }
-      }
     }
-  },
-  "components": {
-    "schemas": {
-      "User": {
-        "type": "object",
-        "required": [
-          "id",
-          "name"
-        ],
-        "properties": {
-          "id": {
-            "type": "integer",
-            "format": "int64"
-          },
-          "name": {
-            "type": "string"
-          },
-          "tag": {
-            "type": "string"
-          }
-        }
-      },
-      "Error": {
-        "type": "object",
-        "required": [
-          "code",
-          "message"
-        ],
-        "properties": {
-          "code": {
-            "type": "integer",
-            "format": "int32"
-          },
-          "message": {
-            "type": "string"
-          }
-        }
-      }
-    }
-  }
 }
