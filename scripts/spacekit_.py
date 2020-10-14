b8b231a5612e1fb2d6c862f8a670ae6c9284b591
Hakkeray <rukeine@gmail.com>
2020-10-14 00:15:53 -0700

    successful test run for spacekit_.py

--- scripts/spacekit_.py
+++ scripts/spacekit_.py
@@ -3,8 +3,9 @@ import zipfile
 import numpy as np
 import spacekit
 from spacekit.transformer import Transformer
+from spacekit.builder import Builder
 
-class Preprocess:
+class Prep:
     def __init__(self):
         HOME = os.path.curdir
         DATA = os.path.abspath(HOME+'/data/')
@@ -42,27 +43,32 @@ class Preprocess:
         X_train, X_test = T.babel_fish_dispenser(X_train, X_test)
         return X_train, X_test
 
-# class Model:
-#     def __init__(self, X_train, X_test, y_train, y_test):
-#         self.X_train = X_train
-#         self.X_test = X_test
-#         self.y_train = y_train
-#         self.y_test = y_test
+class Launch:
+    def __init__(self, X_train, X_test, y_train, y_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        builder = Builder(X_train, X_test, y_train, y_test)
+        self.builder = builder
 
-#     def build_model(self):
-#         pass
+    def deploy(self):
+        builder = self.builder
+        cnn = builder.build_cnn()
+        return cnn
 
-#     def fit_model(self):
-#         pass
+    def takeoff(self, cnn):
+        builder = self.builder
+        history = builder.fit_cnn(cnn)
 
 if __name__ == '__main__':
     import spacekit
-    pre = Preprocess()
-    pre.unzip('/exoTrain.csv.zip', '/exoTest.csv.zip')
-    X_train, X_test, y_train, y_test = pre.split_data('/exoTrain.csv', '/exoTest.csv')
-    X_train, X_test = pre.scale_data(X_train, X_test)
-    X_train, X_test = pre.add_filter(X_train, X_test)
+    prep = Prep()
+    prep.unzip('/exoTrain.csv.zip', '/exoTest.csv.zip')
+    X_train, X_test, y_train, y_test = prep.split_data('/exoTrain.csv', '/exoTest.csv')
+    X_train, X_test = prep.scale_data(X_train, X_test)
+    X_train, X_test = prep.add_filter(X_train, X_test)
 
-    # M = Model()
-    # model = M.build_model(X_train, X_test, y_train, y_test)
-    # M.fit_model(model)
+    launch = Launch(X_train, X_test,  y_train, y_test)
+    cnn = launch.deploy()
+    history = launch.takeoff(cnn)
